b0faa0ab0246e47af3869547cc799090ef9d0eb9
jtn639@gmail.com <jtn639@gmail.com>
2022-08-26 23:30:51 +0900

    프로그래머스 Lv. 2 두 큐 합 같게 만들기 문제 제출
    2022-08-26(금) 1문제 제출

--- src/main/java/programmers/lv2/MakeTwoQueueSumEqual.java
+++ src/main/java/programmers/lv2/MakeTwoQueueSumEqual.java
@@ -4,6 +4,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class MakeTwoQueueSumEqual {
+
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+        int[] queue1 = {1, 1};
+        int[] queue2 = {1, 5};
+        System.out.println(s.solution(queue1, queue2));
+    }
 }
 
 class Solution {
@@ -40,35 +48,42 @@ class Solution {
         Queue<Integer> q2_ori = new LinkedList<Integer>();
         q1_ori.addAll(q1);
         q2_ori.addAll(q2);
+        System.out.println("q1 : " + q1);
+        System.out.println("q2 : " + q2);
+        System.out.println("q1_ori : " + q1_ori);
+        System.out.println("q2_ori : " + q2_ori);
 
-        if (n1Sum < n2Sum) {
+        answer = 0;
+        while (true) {
 
-            while (true) {
+            while (n1Sum < n2Sum) {
 
-                while (n1Sum < n2Sum) {
+                int n2Poll = q2.poll();
+                n2Sum -= n2Poll;
+                q1.add(n2Poll);
+                n1Sum += n2Poll;
+                answer++;
+            }
+            System.out.println("n1Sum : " + n1Sum);
 
-                    int n2Poll = q2.poll();
-                    n2Sum -= n2Poll;
-                    q1.add(n2Poll);
-                    n1Sum += n2Poll;
-                }
+            while (n1Sum > n2Sum) {
 
-                while (n1Sum > n2Sum) {
+                int n1Poll = q1.poll();
+                n1Sum -= n1Poll;
+                q2.add(n1Poll);
+                n2Sum += n1Poll;
+                answer++;
+            }
+            System.out.println("n2Sum : " + n2Sum);
 
-                    int n1Poll = q1.poll();
-                    n1Sum -= n1Poll;
-                    q2.add(n1Poll);
-                    n2Sum += n1Poll;
-                }
+            if (n1Sum == n2Sum) {
+                break;
+            }
 
-                if (n1Sum == n2Sum) {
-                    break;
-                }
-
-                // q1과 q2 요소가 교환되므로 q1은 q2_ori와 q2는 q1_ori와 비교해야 한다.
-                if (q1.equals(q2_ori) && q2.equals(q1_ori)) {
-                    break;
-                }
+            // q1과 q2 요소가 교환되므로 q1은 q2_ori와 q2는 q1_ori와 비교해야 한다.
+            if ((q1.equals(q2_ori) && q2.equals(q1_ori)) || (q1.isEmpty() || q2.isEmpty())) {
+                answer = -1;
+                break;
             }
         }
 
